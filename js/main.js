a1d602ea63eebd5dc143a396b48da0de015067c1
asokolov <asokolov@youa.vn.ua>
2017-11-13 17:25:11 +0200

    mobile menu added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
-console.log("js ready");
 $('.slider').slick();
+
+function openMenu() {
+    var menu = document.getElementById("menu");
+    var body = document.getElementsByTagName("body");
+
+    menu.classList.add("open-menu");
+    body[0].classList.add("no-scroll");
+}
+
+function closeMenu() {
+    var menu = document.getElementById("menu");
+    var body = document.getElementsByTagName("body");
+
+    menu.classList.remove("open-menu");
+    body[0].classList.remove("no-scroll");
+}
